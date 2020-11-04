b395c2c540c44ef06a0a4dc8f008aa159ef14e73
lu <1625196521@qq.com>
2020-11-04 23:53:57 +0800

    user service

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/service/UserService.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/service/UserService.java
@@ -1,7 +1,29 @@
 package com.thoughtworks.capacity.gtb.mvc.service;
 
+import com.thoughtworks.capacity.gtb.mvc.domain.User;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
+    List<User> users = new ArrayList<>();
+
+    public void register(User user) {
+        if (users.stream().anyMatch(e -> e.getUserName().equals(user.getUserName()))) {
+            //todo throw exception
+        }
+        users.add(user);
+    }
+
+    public User login(String userName, String password) {
+        List<User> user1 = users.stream().filter(u -> u.getUserName().equals(userName) && u.getPassword().equals(password))
+                .collect(Collectors.toList());
+        if (user1.size() != 1) {
+            //todo throw exception
+        }
+        return user1.get(0);
+    }
 }
